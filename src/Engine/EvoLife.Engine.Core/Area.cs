d5616d999db7393ade5006b5395d8d0b889ebc51
Piotr Paszko <piotr.paszko.dev@gmail.com>
2020-06-13 12:38:44 +0200

    Add Area type

---
+++ src/Engine/EvoLife.Engine.Core/Area.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EvoLife.Engine.Core
+{
+    /// <summary>
+    /// Class representing an circle area on board
+    /// </summary>
+    public class Area
+    {
+        public Area(Position position, float radius)
+        {
+            Position = position ?? throw new ArgumentNullException(nameof(position));
+            Radius = radius;
+        }
+
+        public Position Position { get; }
+        public float Radius { get; }
+
+        public bool Contains(Position point)
+            => MathF.Pow(point.X - Position.X, 2) + MathF.Pow(point.Y - Position.Y, 2) <=
+               MathF.Pow(Radius, 2);
+
+        public bool Collide(Area area)
+            => MathF.Pow(area.Position.X - Position.X, 2) + MathF.Pow(Position.Y - area.Position.Y, 2) <=
+               MathF.Pow(Radius + area.Radius, 2);
+    }
+}
