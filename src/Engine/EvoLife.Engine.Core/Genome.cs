1ad149891e7cc6e39cb22dac902132e91c7f53d1
Piotr Paszko <piotr.paszko.dev@gmail.com>
2020-06-14 23:11:30 +0200

    Add tests for CellMoveCalculator

--- src/Engine/EvoLife.Engine.Core/Genome.cs
+++ src/Engine/EvoLife.Engine.Core/Genome.cs
@@ -5,20 +5,29 @@
     /// </summary>
     public class Genome
     {
+        public Genome(CellType cellType, float speed, float energyEfficiency)
+        {
+            CellType = cellType;
+            Speed = speed;
+            EnergyEfficiency = energyEfficiency;
+        }
+
         /// <summary>
         /// Type of cell
         /// </summary>
         public CellType CellType { get; }
         
         /// <summary>
-        /// Speed of cell.
+        /// Speed of cell (distance travel in one tick).
         /// The faster the cell is, the less efficient in energy consumption it is.
+        /// Value 0 - 1
         /// </summary>
         public float Speed { get; }
         
         /// <summary>
         /// Energy Efficiency of cell.
         /// The more cell is efficient in energy conversion, the more massive it is.
+        /// Value 0 - 1
         /// </summary>
         public float EnergyEfficiency { get; }
     }
